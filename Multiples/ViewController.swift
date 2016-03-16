2087cfa41b434bf2f0188dec16bc92816327f9a0
sakshyamdahal <dahalsakshyam@gmail.com>
2016-03-16 16:17:16 -0400

    finish the app

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var multipleOf: UITextField!
+    @IBOutlet weak var multipleLogo: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var howManyMultiples = 10
+    var addThisNumber = 0
+    var totalTillNow = 0 {
+        willSet {
+            resultLabel.text = "\(totalTillNow) + \(addThisNumber) = \(totalTillNow + addThisNumber)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func playMultiple(sender: UIButton) {
+        if let multipleValue = multipleOf.text {
+            if let digitalValue = Int(multipleValue) {
+                toggleViews()
+                addThisNumber = digitalValue
+            }
+        }
     }
-
-
+    
+    func toggleViews() {
+        multipleOf.hidden = !multipleOf.hidden
+        playButton.hidden = !playButton.hidden
+        multipleLogo.hidden = !multipleLogo.hidden
+        addButton.hidden = !addButton.hidden
+        resultLabel.hidden = !resultLabel.hidden
+    }
+    
+    @IBAction func addMultiple(sender: UIButton) {
+        if totalTillNow >= addThisNumber * howManyMultiples {
+            addThisNumber = 0
+            totalTillNow = 0
+            toggleViews()
+            resultLabel.text = "Press Add to add!"
+            multipleOf.text = ""
+        } else {
+            totalTillNow += addThisNumber
+        }
+    }
 }
 
